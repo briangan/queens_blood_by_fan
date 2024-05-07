1725bd1999f3988892c73d62329800ea88d152b1
Brian Gan <briangangster@gmail.com>
2024-05-06 23:57:46 -0400

    Card board and cards list admin.

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+//= require 'best_in_place'
+
+$(function() {
+  $(".best-in-place").best_in_place();
+});
---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,20 @@
+class CardsController < InheritedResources::Base
+  def index
+    @cards = Card.includes(:card_tiles, :card_abilities).order(:card_number).page(params[:page])
+    respond_to do |format|
+      format.html
+      format.json { render json: @cards }
+    end
+  end
+
+  def update
+    super do |format|
+      format.js {  }
+      format.json { render json: @card }
+    end
+  end
+
+  def permitted_params
+    params.permit! # (card: [:name, :category, :card_number, :description, :pawn_rank, :power, :raise_pawn_rank] )
+  end
+end
