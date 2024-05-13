9d16b8bcfbef7db06399f1d89f194e1e19adcf24
Brian Gan <briangangster@gmail.com>
2024-05-12 22:26:06 -0400

    Javascript: for cards page, use of Bootstrap tips and CSS alignments & spacing.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def app_name
     t('app_name')
   end
+
+  ##
+  # Parts joined together: $app_name - ($collection or $record_name)
+  def full_page_title
+    s = app_name
+    if respond_to?(:collection) && (first_of_collection = collection.first)
+      s << " - #{first_of_collection.class.model_name.plural.titleize}, Page #{collection.current_page}"
+    elsif respond_to?(:resource) && resource
+      s << " - #{resource.class.model_name.human}"
+    end
+    s
+  end
 end
