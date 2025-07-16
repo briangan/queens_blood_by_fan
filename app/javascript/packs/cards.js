51fc9cf8b7cc9e4a91337d87fa5ba4ad8db2e3ec
Brian Gan <briangangster@gmail.com>
2025-07-16 11:07:26 -0400

    Cards: Javascript moved to /app/javascripts/cards.js pack

---
+++ app/javascript/packs/cards.js
@@ -0,0 +1,138 @@
+var mainBoard = null;
+function getMainBoard() {
+  if (mainBoard){ return mainBoard; }
+  mainBoard = $(".board-wrapper").first();
+  return mainBoard;
+}
+/* Status and questionaire functions ******************/
+function isCardAcceptableToTile(card, tile) {
+  var isAcceptable = ( card.attr('data-player') == tile.attr('data-player') );
+  /////////console.log("Card accept? " + card.attr('data-player') + " vs " + tile.attr('data-player') + " => " + isAcceptable );
+  return isAcceptable;
+}
+function shouldDragRevertDragToTile() {
+  //////////console.log("Should revert? " + $(this).attr('id') + " w/ " + $(this).attr('class') );
+  return !$(this).hasClass('card-dropped');
+}
+
+/* Action Handlers *************************************/
+function dropCardHandler(event, ui) {
+  var shouldAccept = isCardAcceptableToTile( ui.draggable, $(this));
+  // console.log("Card dropped " + ui.draggable.attr('id') + " on " + $(this).attr('id') + " => "+ shouldAccept );
+
+  if (shouldAccept) {
+    ui.draggable.data('board-tile-id', $(this).attr('id') );
+    ui.draggable.draggable( 'disable' );
+    //$(this).droppable( 'disable' );
+    //ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+    var whichPlayer = ui.draggable.data('player');
+    $(this).html('');
+    $(ui.draggable).detach().appendTo( $(this) );
+    ui.draggable.addClass('card-dropped');
+    ui.draggable.removeClass('small-card');
+
+    ui.draggable.draggable( 'option', 'revert', false );
+
+    $(this).attr('data-claiming-player', whichPlayer ); // If drop onto pawn, would be unnecessary
+    resetHLTiles();
+  } else {
+    console.log("> Cannot drop this card here!");
+    resetHLTiles();
+  }
+}
+/* Dry run that only renders the effects of the card placement.
+* Iterates over the pawn-tiles and affected-tiles of the card to highlight the tiles that would be affected.
+*/
+function previewCardPlacementEffect(board, tile, card) {
+  var shouldAccept = isCardAcceptableToTile( card, tile );
+  //////////// console.log("Card hover - preview " + card.attr('data-player') + " on " + tile.attr('data-player') + " => "+ shouldAccept );
+  if (!shouldAccept) {
+    tile.find(".disabled-icon").removeClass('d-none');
+    return; 
+  }
+
+  var pawnTiles = card.data('pawn-tiles');
+  var affectedTiles = card.data('affected-tiles');
+  var abilities = card.data('ability-effects');
+  if (typeof(abilities) == 'string' && abilities !='' ){ 
+    abilities = eval(abilities); 
+  }
+  
+  tile.addClass('center-tile');
+  var tileX = parseInt(tile.data('tile-position').split(',')[0]);
+  var tileY = parseInt(tile.data('tile-position').split(',')[1]);
+  if (typeof(pawnTiles) == 'string'){ pawnTiles = eval(pawnTiles); }
+  pawnTiles.forEach(function(pawnTile) {
+    var col = tileX + pawnTile[0];
+    var row = tileY + pawnTile[1];
+    var targetTile = board.find('.board-tile[data-tile-position="' + col + ',' + row + '"]');
+    if (targetTile.length > 0) {
+      targetTile.addClass('highlight-tile');
+      targetTile.children('.card').addClass('highlight-tile');
+    }
+  } );
+  if (typeof(affectedTiles) == 'string'){ affectedTiles = eval(affectedTiles); }
+  affectedTiles.forEach(function(aTile) {
+    var col = tileX + aTile[0];
+    var row = tileY + aTile[1];
+    var targetTile = board.find('.board-tile[data-tile-position="' + col + ',' + row + '"]');
+    if (targetTile.length > 0) {
+      abilities.forEach(function(ability) {
+        console.log("Preview ability effect to " + targetTile.attr('id') + " => " + ability['action_typeß'] );
+        previewVisualEffectTo(tile, targetTile, ability);
+      });
+    }
+  } );
+}
+function resetHLTiles() {
+  var board = getMainBoard();
+  board.find('.highlight-tile').removeClass('highlight-tile');
+  board.find('.center-tile').find(".disabled-icon").addClass('d-none');
+  board.find('.center-tile').removeClass('center-tile');
+  board.find('.preview-effect-label').each(function(n){ $(this).remove() });
+}
+/* 
+* Renders the visual effect of the card placement on the target tile based on ability.
+*/
+function previewVisualEffectTo(sourceTile, targetTile, ability) 
+{
+  if (ability['action_type'] == 'power_up') {
+    targetTile.children().append("<h3 class='preview-effect-label powerup-effect'> +" + ability['action_value'] + "</h3>")
+  } else if (ability['action_type'] == 'power_down') {
+    targetTile.children().append("<h3 class='preview-effect-label powerdown-effect'> -" + ability['action_value'] + "</h3>")
+  }
+}
+
+// Initialization
+$(function() {
+  $(".draggable").draggable({ 
+    "opacity": 0.35, revert: false,
+    snap: ".board-tile", snapMode: "inner", snapTolerance: 30,
+    stack: ".card-wrapper div",
+    revert: shouldDragRevertDragToTile
+  });
+  $(".droppable").droppable({
+    hoverClass: "card-drop-hover",
+    accept: '.card',
+    drop: dropCardHandler, 
+    over: function(event, ui) {
+      /////// console.log("Card over " + $(this).attr('id'));
+      //$(ui.draggable).children(".bi").removeClass('d-none');
+      $(this).addClass('center-tile');
+      previewCardPlacementEffect( getMainBoard(), $(this), $(ui.draggable) );
+    },
+    out: function(event, ui) {
+      /////// console.log("Card out " + $(this).attr('id'));
+      //$(ui.draggable).children(".bi").addClass('d-none');
+      resetHLTiles();
+    }
+  });
+});
+
+/* $(document).on("dragstart", "card", function (event) {
+  console.log("Card dragging " + $(this).attr('id') );
+
+}); */
+$(document).on("dragstop", ".card", function (event) {
+  $(this).children(".bi").addClass('d-none');
+});
