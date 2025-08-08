e19f7b73bc08483192a2691936b2721d06654394
Brian Gan <briangangster@gmail.com>
2025-08-07 20:56:03 -0400

    DB: migration to correct EnfeebleAbility power_value_change negative

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_06_042423) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_08_005503) do
   create_table "board_tiles", force: :cascade do |t|
     t.integer "board_id", null: false
     t.integer "column", null: false
