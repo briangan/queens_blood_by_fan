fb2c3aa64ecc985bf4426c96d541f8cce2734ec8
Brian Gan <briangangster@gmail.com>
2025-08-13 23:50:25 -0400

    Card: given glowing power; updated EER w/ parent_card_id.  Player tip added w/ pawn icon

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -5,6 +5,12 @@ module CardsHelper
     { card_id: card.id, pawn_tiles: card.pawn_tiles_data(which_player), affected_tiles: card.affected_tiles_data(which_player), ability_effects: card.ability_effects_data, pawn_rank: card.pawn_rank, power: card.power }
   end
 
+  def custom_power_icon(game_board_tile)
+    content_tag :span, class: "custom-power-icon#{' enhanced-glowing-border' if game_board_tile.enhanced?}#{' enfeebled-glowing-border' if game_board_tile.enfeebled?}" do
+      game_board_tile.power_value.to_s
+    end
+  end
+
   def preview_effect_label_css_class(power_value_total_change)
     return 'preview-effect-label' if power_value_total_change.nil? || power_value_total_change == 0
     "preview-effect-label power#{power_value_total_change < 0 ? 'down' : 'up'}-effect"
