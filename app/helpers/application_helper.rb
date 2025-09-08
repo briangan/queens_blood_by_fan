1a514099aaba82d15ae861f41aeab7251bda3e07
Brian Gan <briangangster@gmail.com>
2025-09-07 21:57:20 -0400

    Javascript: added method full_javascript_pack_tag instead of multiple calls of javascript_pack_tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,23 @@ module ApplicationHelper
     s << " - #{@page_title_suffix}" if @page_title_suffix.present?
     s
   end
+
+  # Collect additional javascript packs to be included in the layout.
+  # So later can render them all at once using one javascript_pack_tag call.
+  # Usage: add_to_javascript_packs 'pack_name1', 'pack_name2
+  def add_to_javascript_packs(*packs)
+    @additional_javascript_packs ||= []
+    @additional_javascript_packs.concat(packs)
+  end
+
+  ##
+  # Because of multiple calls of javascript_pack_tag would raise duplicate pack error,
+  # we need to collect all packs and render them at once.
+  # So, in layout file, use <%= full_javascript_packs_tag %> instead of javascript_pack_tag.
+  # At default, 'application' pack is always included.
+  def full_javascript_packs_tag
+    packs = ['application']
+    packs.concat(@additional_javascript_packs) if @additional_javascript_packs.present?
+    javascript_pack_tag(*packs, 'data-turbolinks-track': 'reload', 'as': 'script')
+  end
 end
