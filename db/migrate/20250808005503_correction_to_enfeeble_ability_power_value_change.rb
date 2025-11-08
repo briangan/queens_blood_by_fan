1e57fddac53e1785f927f09c4378e81a74ceaa8c
Brian Gan <briangangster@gmail.com>
2025-11-08 16:04:04 -0500

    Database: fixed wrong old class name of AffectedTileToAbility in db migration

--- db/migrate/20250808005503_correction_to_enfeeble_ability_power_value_change.rb
+++ db/migrate/20250808005503_correction_to_enfeeble_ability_power_value_change.rb
@@ -1,5 +1,5 @@
 class CorrectionToEnfeebleAbilityPowerValueChange < ActiveRecord::Migration[7.2]
   def change
-    GameBoardTileAbility.joins(:card_ability).where("type='EnfeebleAbility' and power_value_change > 0").update_all("power_value_change=-1*power_value_change")
+    AffectedTileToAbility.joins(:card_ability).where("type='EnfeebleAbility' and power_value_change > 0").update_all("power_value_change=-1*power_value_change")
   end
 end
